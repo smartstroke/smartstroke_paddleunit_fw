0d9e1f3969314e3078079f880d03a895258f67d8
William <williamzhang8@live.ca>
2023-04-10 23:56:27 -0700

    Zach: added all data accl/gyro/adc/time

--- smartstroke_paddleunit_fw.ino
+++ smartstroke_paddleunit_fw.ino
@@ -2,19 +2,27 @@
 #include <WiFi.h>
 
 // REPLACE WITH THE RECEIVER'S MAC Address
-uint8_t basestationAddress[] = {0xcc, 0xdb, 0xa7, 0x14, 0x35, 0x38};
+uint8_t basestationAddress[] = {0x8c, 0xaa, 0xb5, 0x8c, 0x4a, 0x38};
 
 // Structure example to send data 
 // Must match the receiver structure
 typedef struct struct_message {
     int id; // must be unique for each sender board
-    int x;
-    int y;
+    long int time;
+    int AccX;
+    int AccY;
+    int AccZ;
+    int ADC;
+    int GyroX;
+    int GyroY;
+    int GyroZ;
 } struct_message;
 
 // Create a struct_message called myData
 struct_message myData;
-
+//time is 32 bit int
+// 7 data values 16 bit
+// Each data point is 
 // Create peer interface
 esp_now_peer_info_t peerInfo;
 
@@ -25,6 +33,7 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
 }
 
 void setup() {
+  Serial.println("Setup");
   // Init Serial Monitor
   Serial.begin(115200);
 
@@ -32,7 +41,7 @@ void setup() {
   WiFi.mode(WIFI_STA);
   Serial.print("ESP Board MAC Address:  ");
   Serial.println(WiFi.macAddress());
-  // Init ESP-NOW
+  //Init ESP-NOW
   if (esp_now_init() != ESP_OK) {
     Serial.println("Error initializing ESP-NOW");
     return;
@@ -46,7 +55,7 @@ void setup() {
   memcpy(peerInfo.peer_addr, basestationAddress, 6);
   peerInfo.channel = 0;
   peerInfo.encrypt = false;
-  // Add peer        
+  //Add peer        
   if (esp_now_add_peer(&peerInfo) != ESP_OK){
     Serial.println("Failed to add peer");
     return;
@@ -54,14 +63,20 @@ void setup() {
 }
 
 void loop() {
+  Serial.println("Loop");
   // Set values to send
   myData.id = 1;
-  myData.x = 10;
-  myData.y = 20;
-  
+  myData.time = 12.001;
+  myData.AccX = 11.234; 
+  myData.AccY = -10.566; 
+  myData.AccZ = 7.401; 
+  myData.ADC = 13.401; 
+  myData.GyroX = 34.234; 
+  myData.GyroY = 21.566; 
+  myData.GyroZ = 9.401; 
   // Send message via ESP-NOW
   esp_err_t result = esp_now_send(basestationAddress, (uint8_t *) &myData, sizeof(myData));
-   
+  
   if (result == ESP_OK) {      
     Serial.println("Sent with success");
   }
